1f25ce8d3ac086940ad639d617d0418fff519013
connector <connector>
2022-09-28 00:20:41 +0200

    Authentication with cookies & localhost backend address

--- src/connector/backend_connector.py
+++ src/connector/backend_connector.py
@@ -1,5 +1,7 @@
 import json
+import time
 import logging as logger
+import requests
 from dataclasses import dataclass
 from distutils.command.config import config
 from http.client import HTTPConnection
@@ -39,26 +41,49 @@ class BackendConnectorBase(ABC):
 @dataclass
 class BackendConnector(BackendConnectorBase):
     config: BackendConfig
+    session: requests.Session = None
+    addr: str = ''
 
+    def __post_init__(self):
+        self.addr = f'http://{self.config.addr}:{self.config.port}'
+        self.login() 
+
+    def login(self):
+        self.session = requests.Session()
+        logger.debug("Sending login request...")
+        data = {"email": "connector", "password": "1qaz098"}
+        try:
+            req = self.session.post(f"{self.addr}/login", data=json.dumps(data))
+            if req.status_code>=200 and req.status_code<300:
+                print("Logged in successfully.")
+        except:
+            print("Major error. Retrying in 5 seconds...")
+            time.sleep(5)
+            self.login()
+
+
+    def logout(self):
+        self.session.get(f"{self.addr}/logout")
+        self.login()
 
     def push_metrics(self, metrics: List[MetricsData]) -> None:
-        httpConnection: HTTPConnection = HTTPConnection(
-            self.config.addr, self.config.port
-        )
         body = MetricsList(
             workstation_name=self.config.workstation_name, metrics=metrics
         )
         body = body.json()
         try:
-            httpConnection.request("POST", "/metrics", body)
+            response = self.session.post(f"{self.addr}/metrics", body)
+            logger.debug(f"Response: {response}")
         except Exception as e:
             print(f"Error pushing metrics {e}")
             raise PushMetricsFailed(e)
-        response = httpConnection.getresponse()
-        data = response.read()
-        if response.status > 299:
+
+        if response.status_code == 401:
+            print("Reauth procedure...")
+            self.logout()
+        elif response.status_code > 299:
             print(f"Error pushing metrics")
-            raise PushMetricsFailed(f"code: {response.status}, detail: {data}")
+            raise PushMetricsFailed(f"code: {response.status_code}")
 
 
 @dataclass
